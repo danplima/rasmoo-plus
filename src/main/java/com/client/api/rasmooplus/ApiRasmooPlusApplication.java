990553e66718a658e052a3b8d2f2313ea96872fe
danplima <danplima25@gmail.com>
2023-04-07 19:10:06 -0400

    Commit Inicial

---
+++ src/main/java/com/client/api/rasmooplus/ApiRasmooPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.api.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRasmooPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRasmooPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/api/rasmooplus/ApiRasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.api.rasmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
